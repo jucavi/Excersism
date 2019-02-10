3f5cfeb2601a1263e853a0b10ec5bec93014084f
Jucavi <jcarvil@gmail.com>
2019-02-10 19:33:39 +0100

    Add higth scores  problem

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -6,20 +6,23 @@ class HighScores
   end
 
   def latest
-    @scores.last
+    scores.last
   end
 
   def personal_best
-    @scores.max
+    scores.max
   end
 
   def personal_top
-    @scores.sort.reverse[0,3]
+    scores.max(3)
   end
 
   def report
     rep = "Your latest score was #{latest}. That's "
-    latest == personal_best ? rep += "your personal best!" : rep += "#{personal_best - latest} short of your personal best!"
-    rep
+    if latest == personal_best
+      rep += "your personal best!"
+    else
+      rep += "#{personal_best - latest} short of your personal best!"
+    end
   end
 end
