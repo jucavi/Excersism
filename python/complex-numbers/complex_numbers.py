a9856a2a8b8086846ea85730fbe0bb06c9cb406c
Jucavi <jcarvil@gmail.com>
2021-08-15 12:24:20 +0200

    Add complex_numbers

---
+++ python/complex-numbers/complex_numbers.py
@@ -0,0 +1,40 @@
+class ComplexNumber:
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+
+    def __eq__(self, other):
+        return (self.real == other.real and self.imaginary == other.imaginary)
+
+    def __add__(self, other):
+        return ComplexNumber(self.real + other.real, self.imaginary + other.imaginary)
+
+    def __mul__(self, other):
+        real = self.real * other.real - self.imaginary * other.imaginary
+        imaginary = self.imaginary * other.real + self.real * other.imaginary
+        return ComplexNumber(real, imaginary)
+
+    def __sub__(self, other):
+        return ComplexNumber(self.real - other.real, self.imaginary - other.imaginary)
+
+    def __truediv__(self, other):      
+        if (self.imaginary != 0 or other.imaginary != 0):
+            denominator = other.real**2 + other.imaginary**2
+            real = (self.real * other.real + self.imaginary * other.imaginary) / denominator
+            imaginary = (self.imaginary * other.real - self.real * other.imaginary) / denominator
+            return ComplexNumber(real, imaginary)
+
+        return ComplexNumber(self.real / other.real, 0)
+            
+    def __abs__(self):
+        import math
+        return math.sqrt(self.real**2 + self.imaginary**2)
+        
+    def conjugate(self):
+        return ComplexNumber(self.real, -self.imaginary)
+
+    def exp(self):
+        import math
+        m = math.e**self.real
+        
+        return ComplexNumber(m*math.cos(self.imaginary), m*math.sin(self.imaginary))
