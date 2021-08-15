60daaf6bd01f299b512a6bb0b701596e50496043
Jucavi <jcarvil@gmail.com>
2021-08-15 13:25:20 +0200

    Refactor complex_numbers

--- python/complex-numbers/complex_numbers.py
+++ python/complex-numbers/complex_numbers.py
@@ -1,3 +1,5 @@
+import math
+
 class ComplexNumber:
     def __init__(self, real, imaginary):
         self.real = real
@@ -20,21 +22,21 @@ class ComplexNumber:
     def __truediv__(self, other):      
         if (self.imaginary != 0 or other.imaginary != 0):
             denominator = other.real**2 + other.imaginary**2
-            real = (self.real * other.real + self.imaginary * other.imaginary) / denominator
-            imaginary = (self.imaginary * other.real - self.real * other.imaginary) / denominator
-            return ComplexNumber(real, imaginary)
+            real, imaginary = self * other.conjugate()
+            return ComplexNumber(real / denominator, imaginary / denominator)
 
         return ComplexNumber(self.real / other.real, 0)
             
     def __abs__(self):
-        import math
         return math.sqrt(self.real**2 + self.imaginary**2)
+    
+    def __iter__(self):
+        return iter((self.real, self.imaginary))
         
     def conjugate(self):
         return ComplexNumber(self.real, -self.imaginary)
 
     def exp(self):
-        import math
         m = math.e**self.real
         
         return ComplexNumber(m*math.cos(self.imaginary), m*math.sin(self.imaginary))
